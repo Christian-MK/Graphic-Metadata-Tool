acc0f40f35f25305bf3684a27d01a04b270843d9
Christian-MK <105018870+Christian-MK@users.noreply.github.com>
2022-11-16 11:27:25 -0800

    Update GroupProject_V1.py

--- GroupProject_V1.py
+++ GroupProject_V1.py
@@ -3,20 +3,19 @@
 #Using Pillow to extract
 #https://medium.com/nerd-for-tech/script-to-extract-image-metadata-using-python-and-pillow-library-53a6ae56ccc3
 
-
 # Photo metadata is a set of data describing and providing information about rights and administration of an image.
 
-from datetime import date       #https://stackoverflow.com/questions/32490629/getting-todays-date-in-yyyy-mm-dd-in-python
-from datetime import datetime
+from datetime import date       #Adds current date for auditability
+from datetime import datetime   #Adds current time for auditability
 today = str(date.today())
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-#Adds current date and time for auditability
+#https://stackoverflow.com/questions/32490629/getting-todays-date-in-yyyy-mm-dd-in-python
+
 import os
-from PIL import Image
-# Importing Pillow Python Imaging Library:that adds support for opening, manipulating, and saving many different image file formats.
+from PIL import Image   # Importing Pillow Python Imaging Library adds support for opening, manipulating, and saving many different image file formats.
 from PIL.ExifTags import TAGS
-#script = GroupProject_V1.py    #tied to line print function
+
 print('======================================================================')
 print('\n  Welcome to the Image Archival tool developed by Christian & Kelso.')
 while True:
@@ -32,40 +31,40 @@ while True:
     print('\n----------------------------------------------------------------------')
     choice = input('Choice: ')
 
-    if choice == '1':
+    if choice == '1':   #New File: Print Metadata
         image_file = input('  Enter File Name:\n    ')
         print("----------------------------------------------------------------------")
         try:
-            image = Image.open(image_file)
+            image = Image.open(image_file)  #Prevents errors due to invalid inputs
         except:
             print('  Invalid Entry.')
             print('    Please Restart.')
             quit()
-        # dictionary to store metadata keys and value pairs.
-        exif = {}
-        # iterating over the dictionary
-        for tag, value in image._getexif().items():
-        #extarcting all the metadata as key and value pairs and converting them from numerical value to string values
+       
+        exif = {}    # dictionary to store metadata keys and value pairs.
+       
+        for tag, value in image._getexif().items():  # iterating over the dictionary
+       
             if tag in TAGS:
-                exif[TAGS[tag]] = value
-        #checking if image is copyrighted
+                exif[TAGS[tag]] = value  #extarcting all the metadata as key and value pairs and converting them from numerical value to string values
+       
         try:
-            if 'Copyright' in exif:
+            if 'Copyright' in exif:  #checking if image is copyrighted
                 print("Image is Copyrighted, by ", exif['Copyright'])
         except KeyError:
             pass
 
-        print()
+        print() #   ???whats this for??
         print("Displaying all embedded metadata of object: \n")
         for metadata in exif:
-            print(metadata + ": " + str(exif[metadata]))
-        #print(exif)
+            print(metadata + ": " + str(exif[metadata]))    #Formats output into 2 columns for ease of view
+            #Should this be ordered? Alphabetically?
         pass
 
     elif choice == '2':#Rename
-    #https://pynative.com/python-rename-file/#:~:text=Use%20rename()%20method%20of%20an%20OS%20module&text=rename()%20method%20to%20rename,function%20to%20rename%20a%20file.
         while True:
             old_name = os.path.abspath(image_file)   #we assigned in 1
+             #https://pynative.com/python-rename-file/#:~:text=Use%20rename()%20method%20of%20an%20OS%20module&text=rename()%20method%20to%20rename,function%20to%20rename%20a%20file.
 
             while True:
                 metadata = input('  Metadata name: \n    ')
@@ -86,8 +85,7 @@ while True:
             print("  File renamed!")
 
             continue_rename = input('  Continue Renaming? (T/F) \n    ')
-            # TODO: Check they put T or F
-            if continue_rename != "T" or "t":
+            if continue_rename != "T" or "t":   #Catches if user tries to use capital or no
                 break
             else:
                 image_file = new_name
@@ -99,6 +97,11 @@ while True:
 
     #elif choice == '5': #prints audit
         #script > out.txt     #https://stackoverflow.com/questions/23364096/how-to-write-output-of-terminal-to-file
+        #txt file should be created/opened at the point of image open
+        #all actions executed on file should be printed to file
+        #file closed when choice 1 is selected again
+        #txt file must share name with image
+        #txt name must change with image during option 2
 
     elif choice == '4':#Exit
         print('  Toodles')
